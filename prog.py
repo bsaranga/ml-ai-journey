0dd10efd7138a0030f59ac4cc135f472e488c1b7
bsaranga <bsaranga@gmail.com>
2023-02-19 20:40:05 +0530

    args and kwargs

--- prog.py
+++ prog.py
@@ -5,7 +5,24 @@ i = 0
 while i < len(f):
     print(f[i])
     i+=1
-'''
 
 for c in f:
     print(c)
+'''
+
+def fib(n):
+    '''Print the fibonacci sequence'''
+    a,b = 0,1
+    while a < n:
+        print(a, end=' ')
+        a, b = b, a+b
+    print()
+
+'''fib(10)'''
+
+def func(foo, *args, **kwargs):
+    print(args)
+    print(args[0])
+    print(kwargs['shop'])
+
+func('foo', 1, 2, 3, shop='LIB')
