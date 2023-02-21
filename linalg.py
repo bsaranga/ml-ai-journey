a109f2d59d8df890b70808237ea3b5f308118779
bsaranga <bsaranga@gmail.com>
2023-02-21 08:09:29 +0530

    added completed assignment and unit tests

--- linalg.py
+++ linalg.py
@@ -1,13 +1,28 @@
 import numpy as np
+import w2_unittest
 
 A = np.array([     
         [2, -1, 1, 1],
         [1, 2, -1, -1],
         [-1, 2, 2, 2],
         [1, -1, 2, 1]    
-    ], dtype=np.dtype(float))
+    ], dtype=np.dtype(int))
 
-b = np.array([6, 3, 14, 8], dtype=np.dtype(float))
+b = np.array([6, 3, 14, 8], dtype=np.dtype(int))
+
+### START CODE HERE ###
+# determinant of matrix A
+d = np.linalg.det(A)
+
+# solution of the system of linear equations 
+# with the corresponding coefficients matrix A and free coefficients b
+x = np.linalg.solve(A, b)
+### END CODE HERE ###
+#grade-up-to-here
+#print(f"Determinant of matrix A: {d:.2f}")
+#print(f"Solution vector: {x}")
+
+
 
 def Stack(M, m):
     outArr = np.zeros((4,5))
@@ -41,10 +56,29 @@ def SwapRows(M, row_num_1, row_num_2):
     M_new[row_num_2] = row1
     return M_new
 
+A_test = np.array([
+        [1, -2, 3, -4],
+        [-5, 6, -7, 8],
+        [-4, 3, -2, 1], 
+        [8, -7, 6, -5]
+    ], dtype=np.dtype(float))
+#print("Original matrix:")
+#print(A_test)
+
+#print("\nOriginal matrix after its third row is multiplied by -2:")
+#print(MultiplyRow(A_test,2,-2))
+
+#print("\nOriginal matrix after exchange of the third row with the sum of itself and first row multiplied by 4:")
+#print(AddRows(A_test,0,2,4))
+
+#print("\nOriginal matrix after exchange of its first and third rows:")
+#print(SwapRows(A_test,0,2))
+
+
 def augmented_to_ref(A, b):    
     ### START CODE HERE ###
     def Stack(M, m):
-        outArr = np.zeros((4,5))
+        outArr = np.zeros((4,5), dtype=np.dtype(int))
         MCopy = M.copy()
         for ix, i in enumerate(m):
             outArr[ix] = np.append(MCopy[ix], i)
@@ -93,4 +127,48 @@ def augmented_to_ref(A, b):
 
 A_ref = augmented_to_ref(A, b)
 
-print(A_ref)
+### START CODE HERE ###
+
+# find the value of x_4 from the last line of the reduced matrix A_ref
+x_4 = 1
+
+# find the value of x_3 from the previous row of the matrix. Use value of x_4.
+x_3 = 7-3*x_4
+
+# find the value of x_2 from the second row of the matrix. Use values of x_3 and x_4
+x_2 = 22-3*x_4-4*x_3
+
+# find the value of x_1 from the first row of the matrix. Use values of x_2, x_3 and x_4
+x_1 = 3+x_4+x_3-2*x_2
+### END CODE HERE ###
+
+#print(x_1, x_2, x_3, x_4)
+
+
+def ref_to_diagonal(A_ref):    
+    ### START CODE HERE ###
+    # multiply row 3 of the matrix A_ref by -3 and add it to the row 2
+    A_diag = AddRows(A_ref, 3, 2, -3)
+    
+    # multiply row 3 of the new matrix A_diag by -3 and add it to the row 1
+    A_diag = AddRows(A_diag, 3, 1, -3)
+    
+    # add row 3 of the new matrix A_diag to the row 0, replacing row 0
+    A_diag = AddRows(A_diag, 3, 0, 1)
+    
+    # multiply row 2 of the new matrix A_diag by -4 and add it to the row 1
+    A_diag = AddRows(A_diag, 2, 1, -4)
+    
+    # add row 2 of the new matrix A_diag to the row 0, replacing row 0
+    A_diag = AddRows(A_diag, 2, 0, 1)
+    
+    # multiply row 1 of the new matrix A_diag by -2 and add it to the row 0
+    A_diag = AddRows(A_diag, 1, 0, -2)
+    ### END CODE HERE ###
+    
+    return A_diag
+    
+A_diag = ref_to_diagonal(A_ref)
+
+#print(A_diag)
+w2_unittest.test_augmented_to_ref(augmented_to_ref)
