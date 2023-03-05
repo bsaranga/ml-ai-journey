350032d8ef6574f3e19f824765306a39eef42a60
bsaranga <bsaranga@gmail.com>
2023-03-05 13:05:05 +0530

    3d plots

--- 3dplots/surf.py
+++ 3dplots/surf.py
@@ -1,4 +1,23 @@
+import matplotlib.pyplot as plt
+from matplotlib import cm
+import numpy as np
+
 from gradient_descent import grad as g
 
-def fn(x):
-    return (x-2)**2 + 2.5
+
+# Make data
+X = np.arange(-5, 5, .1)
+Y = np.arange(-5, 5, .1)
+X, Y = np.meshgrid(X, Y)
+Z = np.sin(abs(X)) + np.cos(abs(Y))
+
+minimum = [0,0,0]
+
+# Plot the surface
+fig, ax = plt.subplots(figsize=(8,8), subplot_kw={"projection": "3d"})
+
+ax.plot_surface(X, Y, Z, vmin=Z.min() * 2, cmap=cm.Blues, zorder=1)
+
+ax.plot(minimum[0], minimum[1], minimum[2], marker='o', color='red', zorder=10)
+
+plt.show()
