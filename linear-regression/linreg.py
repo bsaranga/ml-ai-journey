ed429e2765081d0756dd7d0259dcd00103f35f2b
bsaranga <bsaranga@gmail.com>
2023-03-08 22:09:13 +0530

    generic partial derivative function tested against preliminary data

--- linear-regression/linreg.py
+++ linear-regression/linreg.py
@@ -4,6 +4,8 @@ import matplotlib.pyplot as plt
 # A library for data manipulation and analysis.
 import pandas as pd
 
+from gradient_descent import grad as g
+
 path = '/home/bsaranga/repos/ml-ai-journey/linear-regression/data/tvmarketing.csv'
 adv = pd.read_csv(path)
 
@@ -16,3 +18,8 @@ Y_norm = (Y - np.mean(Y))/np.std(Y)
 # Cost function
 def E(m, b, X, Y):
     return (1/(2*len(Y))) * np.sum((m*X + b - Y)**2)
+
+print(g.partialDerivative(E, [0,0,X_norm,Y_norm], 0))
+print(g.partialDerivative(E, [0,0,X_norm,Y_norm], 1))
+print(g.partialDerivative(E, [1,5,X_norm,Y_norm], 0))
+print(g.partialDerivative(E, [1,5,X_norm,Y_norm], 1))
