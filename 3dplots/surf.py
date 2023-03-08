52fe7497b0ff51d301f6929e3d1ab03d58a05b7e
bsaranga <bsaranga@gmail.com>
2023-03-08 08:31:16 +0530

    gradient descent with path plot

--- 3dplots/surf.py
+++ 3dplots/surf.py
@@ -9,9 +9,11 @@ def fn(x,y):
 def f_example_3(x,y):
     return (85+ 0.1*(- 1/9*(x-6)*x**2*y**3 + 2/3*(x-6)*x**2*y**2))
 
-result = g.gradient_descent2([0.5,0.5], 0.25, f_example_3)
-print(result[0])
-print(result[1])
+result = g.gradient_descent2([2,0.5], 0.26, f_example_3)
+
+minima = result[0]
+path_x_coords, path_y_coords = np.split(result[1], 2, axis=1)
+path_z_coords = f_example_3(path_x_coords, path_y_coords)
 
 # Make data
 X = np.linspace(0, 5, 50)
@@ -27,7 +29,8 @@ ax.set_ylabel('y-axis')
 ax.set_zlabel('z-axis')
 
 ax.plot_surface(X, Y, Z, antialiased=True, cmap='coolwarm', zorder=1)
-ax.plot([0,2,5],[0,2,5],[80,80,80], zorder=10, color='green')
-ax.autoscale(enable=False)
+ax.plot(path_x_coords, path_y_coords, path_z_coords, zorder=10, color='blue')
+ax.plot(path_x_coords, path_y_coords, path_z_coords, '*', zorder=11, color='yellow')
+ax.plot(minima[0], minima[1], f_example_3(minima[0], minima[1]), 'ro', zorder=12)
 
 plt.show()
