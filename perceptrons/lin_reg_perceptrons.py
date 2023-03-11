adb699c19ba286e56125adf2bdf3b88aa93b52a7
bsaranga <bsaranga@gmail.com>
2023-03-11 14:12:22 +0530

    init

---
+++ perceptrons/lin_reg_perceptrons.py
@@ -0,0 +1,7 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+np.random.seed(3)
+
+path = '/home/bsaranga/repos/ml-ai-journey/perceptrons/data/tvmarketing.csv'
+adv = pd.read_csv(path)
