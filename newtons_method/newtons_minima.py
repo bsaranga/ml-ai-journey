89c18102a55799607563c74fc01751855ea72ca4
bsaranga <bsaranga@gmail.com>
2023-03-11 19:18:09 +0530

    minima finding with newton's method

--- newtons_method/newtons_minima.py
+++ newtons_method/newtons_minima.py
@@ -2,29 +2,26 @@ import numpy as np
 import matplotlib.pyplot as plt
 from gradient_descent import grad as g
 
-#x = x_0 - f(x)/f'(x)
-
 x = np.linspace(-2, 7, 100)
 
 def polynomial(x):
     return (x - np.e)**2 - 2*np.pi
 
-def newtons_method_root(f, x0, threshold=0.01):
+def newtons_method_min(f, x0, threshold=0.01):
     x = x0
     iteration = 0
-    while abs(f(x)) >= threshold:
-        x = x - f(x)/g.gradient_at(x, f)
+    while abs(g.gradient_at(x,f)) >= threshold:
+        x = x - g.gradient_at(x, f)/g.second_derivative_at(x, f, epsilon=0.001)
         iteration += 1
 
     print(f"Iterations: {iteration}")
     return x
 
-root1 = newtons_method_root(polynomial, 2)
-root2 = newtons_method_root(polynomial, 4)
+minima = newtons_method_min(polynomial, 6, threshold=0.001)
+print(minima)
 
 plt.plot(x, polynomial(x))
-plt.plot(root1, 0, 'ro')
-plt.plot(root2, 0, 'ro')
+plt.plot(minima, polynomial(minima), 'ro')
 plt.grid(True, 'both')
 plt.axhline(y=0, color='k')
 plt.show()
